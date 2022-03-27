36614729620de5331687fb3f45f270a29a516a67
RRR9 <43386258+RRR9@users.noreply.github.com>
2022-03-27 21:58:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 #include "haffman.h"
 
 int main(void) {
-	setlocale(LC_ALL, "rus");
-	clock_t begin = clock();
-	//codify("c:\\in.txt", "c:\\binary.bin");
-	decifer("c:\\binary.bin", "c:\\output.txt");
-	printf("%.2lf\n", (double)(clock() - begin) / CLOCKS_PER_SEC);
+    setlocale(LC_ALL, "rus");
+    clock_t begin = clock();
+    //codify("c:\\in.txt", "c:\\binary.bin");
+    decifer("c:\\binary.bin", "c:\\output.txt");
+    printf("%.2lf\n", (double)(clock() - begin) / CLOCKS_PER_SEC);
     return 0;
 }
